9654ff1812bdb0e552dfd3a75472b5c7a1ae4f77
nik9play <n-bogun@mail.ru>
2020-06-08 02:45:32 +0300

    spaces to tabs

--- src/App.svelte
+++ src/App.svelte
@@ -6,37 +6,37 @@ import SortableList from './components/SortableList.svelte'
 let bookmarks = []
 
 browser.storage.local.get("bookmarks").then(item => {
-	bookmarks = item.bookmarks
+  bookmarks = item.bookmarks
 })
 
 function handleAdd(e) {
-	bookmarks = e.detail.newBookmarks
+  bookmarks = e.detail.newBookmarks
 }
 
 function handleRemove(e) {
-	bookmarks.splice(e.detail.index, 1)
-	bookmarks = bookmarks
-	browser.storage.local.set({
-		bookmarks: bookmarks
-	})
+  bookmarks.splice(e.detail.index, 1)
+  bookmarks = bookmarks
+  browser.storage.local.set({
+    bookmarks: bookmarks
+  })
 }
 
 const sortList = ev => {
-	bookmarks = ev.detail
-	browser.storage.local.set({
-		bookmarks: bookmarks
-	})
+  bookmarks = ev.detail
+  browser.storage.local.set({
+    bookmarks: bookmarks
+  })
 }
 </script>
 
 <Add on:add={handleAdd}/>
 
 <!-- {#each bookmarks as bookmark, i (bookmark)}
-	<Card on:remove={handleRemove} {...bookmark} index={i}/>
+  <Card on:remove={handleRemove} {...bookmark} index={i}/>
 {/each} -->
 
 <SortableList list={bookmarks} let:item let:index on:sort={sortList}>
-	<Card on:remove={handleRemove} {...item} index={index}/>
+  <Card on:remove={handleRemove} {...item} index={index}/>
 </SortableList>
 
 <style>
